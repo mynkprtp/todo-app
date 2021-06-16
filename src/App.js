b387df1019a780ac2235b16f1a24fa29ce51a9d6
Mayank Pratap Singh <itsmayankhere24@gmail.com>
2021-06-16 12:19:37 +0530

    Authentication Page Front End

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { useState } from "react";
+
+import Auth from "./user/pages/Auth";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let routes;
+  // setIsLoggedIn(false);
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <h1>Home Page</h1>
+        </Route>
+        <Route path="/:userId/tasks" exact>
+          <h1>My tasks</h1>
+        </Route>
+        <Route path="/tasks/new" exact>
+          <h1>New task</h1>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <h1>Home Page</h1>
+        </Route>
+        <Route path="/auth" exact>
+          {/* <h1>Authorize to Enter todo world</h1> */}
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
   return (
     <>
-      <div>1st Div</div>
-      <div className="App">Hello</div>
+      <Router>
+        <main>{routes}</main>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
